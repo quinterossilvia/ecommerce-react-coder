cf87489b99d5997ae56853c2d847ed4def2f4395
Silvia Quinteros <quinterossilvia@yahoo.com.ar>
2023-04-23 20:12:21 -0300

    agrego boton volver

--- src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import {Card, Button} from 'react-bootstrap'
 import ItemCount from './ItemCount'
 import '../ItemDetail/itemDetail.css'
 
+
 export const ItemDetail = ({ id, imagen, descripcion, trago, precio }) => {
+  const navigate = useNavigate()
+
+  const volverHaciaAtras = () =>{
+    navigate(-1)
+  }
+  
+  
+  
   return (
     
 
@@ -15,6 +25,8 @@ export const ItemDetail = ({ id, imagen, descripcion, trago, precio }) => {
         <Card.Title>${precio}</Card.Title>
         <ItemCount />
         <Button variant="secondary">Agregar al Carrito</Button>
+        <Button variant="bg-light"onClick={volverHaciaAtras}className='m-2'>Volver</Button>
+
       </Card.Body>
     </Card>
   )
